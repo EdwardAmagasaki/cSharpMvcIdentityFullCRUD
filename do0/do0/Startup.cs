6c5efdfe957fd188f0c9d2e2dacdf2aafaf9f9dd
Edward Amagasaki <amagasaki.edward@gmail.com>
2023-03-25 12:28:43 -0300

    Primeiro Commit ASP.NET C# MVC

---
+++ do0/do0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(do0.Startup))]
+namespace do0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
